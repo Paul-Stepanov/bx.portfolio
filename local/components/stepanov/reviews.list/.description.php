4e738606861b58fc8bf6b45d400383b5548d3b0b
Stepanov <p.stepanov13@mail.ru>
2023-06-04 02:51:59 +0300

    the reviews component has been created

---
+++ local/components/stepanov/reviews.list/.description.php
@@ -0,0 +1,12 @@
+<?php
+	if( !defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+		die();
+	}
+	$arComponentDescription = [
+		"NAME" => "Список отзывов",
+		"DESCRIPTION" => "Выводим список отзывов с возможностью добавления нового отзыва",
+		"PATH" => [
+			"ID" => "reviews_list",
+			"NAME" => "Отзывы",
+		],
+	];
