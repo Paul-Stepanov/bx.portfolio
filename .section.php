0296e9724ef260dcf6809e532bb6088c0cd1f3cd
Stepanov <p.stepanov13@mail.ru>
2023-06-03 14:00:48 +0300

    start creating a new project

---
+++ .section.php
@@ -0,0 +1,9 @@
+<?
+$sSectionName = 'Главная';
+$arDirProperties = array(
+	'title' => 'Portfolio',
+	'description' => 'My portfolio web site on bitrix framework',
+	'keywords' => 'portfolio',
+	'robots' => 'index, follow'
+);
+?>
