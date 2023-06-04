dd38932f64c412b38ed35404cc3e67c9eb2fc63a
Stepanov <p.stepanov13@mail.ru>
2023-06-04 16:47:12 +0300

    an event handler has been created for the info block element Reviews

---
+++ local/php_interface/classes/MyIBlockHandler.php
@@ -0,0 +1,22 @@
+<?php
+	namespace Stepanov\App;
+
+	use CIBlock;
+
+	class MyIBlockHandler {
+		/**
+		 * @param $arFields
+		 *
+		 * @return void
+		 * /*------------------------------------------------------------------------------------------#
+		 * Обработчик события на запрет корректировки текста или оценки отзыва
+		 * в инфоблоке "Отзывы" ('IBLOCK_CODE' = 'reviews') ↓
+		 * #------------------------------------------------------------------------------------------*/
+		public static function notUpdateCommentAndRating(&$arFields): void {
+
+			$currentIBlock = CIBlock::GetList([], ["ID" => $arFields["IBLOCK_ID"]])->GetNext();
+			if($currentIBlock['CODE'] == 'reviews') {
+				unset($arFields["PROPERTY_VALUES"]);
+			}
+		}
+	}
