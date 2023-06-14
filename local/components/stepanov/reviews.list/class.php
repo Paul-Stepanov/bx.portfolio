8475707cc6bfd48f6658a378d3a851acb7b199f0
Stepanov <p.stepanov13@mail.ru>
2023-06-14 18:03:18 +0300

    refactoring the component ReviewsList

--- local/components/stepanov/reviews.list/class.php
+++ local/components/stepanov/reviews.list/class.php
@@ -4,7 +4,12 @@
         die();
     }
 
-    use Bitrix\Main\{Application, HttpRequest, Loader, LoaderException};
+    use Bitrix\Main\{Application,
+        HttpRequest,
+        Loader,
+        LoaderException,
+        SystemException
+    };
 
     /**------------------------------------------------------------------------------------------#
      * Для использования компонента необходимо создать информационный блок
@@ -26,32 +31,11 @@
         private string|false $navString;
 
         /**
-         * Объект запроса добавления комментария.
-         *
-         * @var HttpRequest
-         */
-        private HttpRequest $commentRequest;
-
-        /**
-         * Объект результата выборки элементов инфоблока.
-         *
-         * @var CIBlockResult
-         */
-        private CIBlockResult $iBlockResult;
-
-        /**
          * Данные информационного блока "отзывы".
          *
          * @var array|bool
          */
-        private array|bool $iBlock;
-
-        /**
-         * Массив элементов инфоблока сформированный после выборки.
-         *
-         * @var array
-         */
-        private array $arElements = [];
+        private array|bool $iBlockReviews;
 
         /**
          * Массив значений для осуществления транслитерации символьного кода
@@ -78,7 +62,7 @@
          *
          * @var array
          */
-        private array $arFieldsComment = [
+        private array $commentFieldsIBlockReviews = [
             "ACTIVE" => "Y",
             "IBLOCK_ID" => "",
             "NAME" => "",
@@ -91,13 +75,6 @@
         ];
 
         /**
-         * Массив полей рейтинга типа значение->ID.
-         *
-         * @var array
-         */
-        private array $ratingEnumFields = [];
-
-        /**
          *------------------------------------------------------------------------------------------#
          * Подготовка параметров компонента ↓
          * ---------------------------------------------------------------------------------------#
@@ -106,44 +83,14 @@
          *
          * @return array
          *
-         * @throws LoaderException
-         *
          */
         public function onPrepareComponentParams($arParams): array
         {
-            Loader::IncludeModule("iblock");
-
-            $this->iBlock = CIBlock::GetList(
-                arOrder: [],
-                arFilter: ["CODE" => "reviews"],
-            )
-                ->GetNext();
-
             $arParams["REVIEWS_COUNT"] = intval($arParams["REVIEWS_COUNT"]);
 
             if ($arParams["REVIEWS_COUNT"] <= 0) {
                 $arParams["REVIEWS_COUNT"] = 6;
             }
-
-            $this->iBlockResult = CIBlockElement::GetList(
-                arOrder: [$arParams["SORT_BY"] => $arParams["SORT_ORDER"]],
-                arFilter: [$this->iBlock["ID"]],
-                arGroupBy: false,
-                arNavStartParams: ["nPageSize" => $arParams["REVIEWS_COUNT"]],
-                arSelectFields: [
-                    "ID",
-                    "IBLOCK_ID",
-                    "IBLOCK_NAME",
-                    "NAME",
-                    "PREVIEW_TEXT",
-                    "DATE_ACTIVE_FROM",
-                    "PROPERTY_COMMENT",
-                    "PROPERTY_RATING",
-                ],
-            );
-
-            $this->commentRequest =
-                Application::getInstance()->getContext()->getRequest();
 
             return $arParams;
         }
@@ -155,88 +102,176 @@
          *
          * @return void
          *
-         * @throws LoaderException
-         *
-         * @throws Exception
-         *
          */
         public function executeComponent(): void
         {
-            if (!$this->checkIBlockReviews()) {
-                ShowError(
-                    strError: "Отсутствует информационный блок 'Отзывы'!
-	                Для использования компонента необходимо создать информационный блок \"Отзывы\",
-	                с символьным кодом: \"reviews\", содержащий свойства:
-	                - \"Комментарий\" (тип - строка, код свойства - \"COMMENT\", обязательное к заполнению);
-	                - \"Оценка\" (тип - список, код свойства - \"RATING\", значения списка от 1 до 5).",
-                );
-            }
-            $this->getIBlockElements($this->iBlockResult);
+            $this->checkIBlockModule();
 
-            $this->arResult["IBLOCK"] = $this->iBlock;
+            $this->getIBlockReviews();
 
-            $this->arResult["ITEMS"] = $this->arElements;
+            $this->checkIBlockReviews($this->iBlockReviews);
 
-            $this->arResult["RATING"] = $this->ratingEnumFields;
+            $iBlockReviewsResultObject = $this->getIBlockReviewsResultObject();
 
-            $this->arResult["NAV_STRING"] = $this->navString;
+            $this->getIBlockReviewsElements($iBlockReviewsResultObject);
 
-            $this->addComment($this->commentRequest);
+            $this->getNavString($iBlockReviewsResultObject);
+
+            $this->getRatingEnumFields();
+
+            $this->addComment($this->getCommentRequestObject());
+
+            $this->arResult["IBLOCK"] = $this->iBlockReviews;
 
             $this->includeComponentTemplate();
         }
 
         /**
-         * ------------------------------------------------------------------------------------------#
-         * Метод проверки наличия модуля Инфоблоки, а также существования
-         * инфоблока "Отзывы", с символьным кодом "reviews" ↓
-         * ----------------------------------------------------------------------------------------#
-         *
-         * @return bool
-         *
-         * @throws LoaderException
-         *
+         *------------------------------------------------------------------------------------------#
+         * Проверка наличие в системе установленного модуля "Информационные
+         * блоки" ↓
+         *----------------------------------------------------------------------------------------#
          */
-        private function checkIBlockReviews(): bool
+        private function checkIBlockModule(): void
         {
-            if (!Loader::IncludeModule("iblock") || !$this->iBlock) {
-                return false;
-            } else {
-                return true;
+            try {
+                Loader::IncludeModule("iblock")
+                    ? true
+                    : throw new LoaderException(
+                    ShowError(
+                        strError: "Не установлен модуль 'Информационные блоки (iblock)'!!!",
+                    )
+                );
+            } catch (LoaderException $exception) {
+                $exception->getMessage();
             }
         }
 
         /**
          *------------------------------------------------------------------------------------------#
-         * Выбираем элементы инфоблока "Отзывы" с необходимыми полями ↓
+         * Запрос данных информационного блока "Отзывы" ↓
+         *----------------------------------------------------------------------------------------#
+         *
+         * @return void
+         */
+        private function getIBlockReviews(): void
+        {
+            $this->iBlockReviews = CIBlock::GetList(
+                arOrder: [],
+                arFilter: ["CODE" => "reviews"],
+            )
+                ->GetNext();
+        }
+
+        /**
+         * ------------------------------------------------------------------------------------------#
+         * Проверка существования инфоблока "Отзывы", с символьным кодом
+         * "reviews" ↓
+         * ----------------------------------------------------------------------------------------#
+         *
+         * @param $iBlockReviews
+         *
+         */
+        private function checkIBlockReviews($iBlockReviews)
+        {
+            try {
+                $iBlockReviews
+                    ? true
+                    : throw new SystemException(
+                    ShowError(
+                        strError: "Отсутствует информационный блок 'Отзывы'!
+                                Для использования компонента необходимо создать информационный блок 'Отзывы',
+                                с символьным кодом: 'reviews', содержащий свойства:
+                                - 'Комментарий' (тип - строка, код свойства - 'COMMENT', обязательное к заполнению);
+                                - 'Оценка' (тип - список, код свойства - 'RATING', значения списка от 1 до 5).",
+                    )
+                );
+            } catch (SystemException $exception) {
+                $exception->getMessage();
+            }
+        }
+
+        /**
+         *------------------------------------------------------------------------------------------#
+         * Получаем экземпляр класса CIBlockResult ↓
          * ---------------------------------------------------------------------------------------#
+         *
+         * @return \CIBlockResult|int
+         */
+        private function getIBlockReviewsResultObject(): CIBlockResult|int
+        {
+            return CIBlockElement::GetList(
+                arOrder: [$this->arParams["SORT_BY"] => $this->arParams["SORT_ORDER"]],
+                arFilter: [$this->iBlockReviews["ID"]],
+                arGroupBy: false,
+                arNavStartParams: ["nPageSize" => $this->arParams["REVIEWS_COUNT"]],
+                arSelectFields: [
+                    "ID",
+                    "IBLOCK_ID",
+                    "IBLOCK_NAME",
+                    "NAME",
+                    "PREVIEW_TEXT",
+                    "DATE_ACTIVE_FROM",
+                    "PROPERTY_COMMENT",
+                    "PROPERTY_RATING",
+                ],
+            );
+        }
+
+        /**
+         *------------------------------------------------------------------------------------------#
+         * Выбираем элементы инфоблока "Отзывы" с необходимыми полями ↓
+         *---------------------------------------------------------------------------------------#
          *
          * @param  CIBlockResult  $iBlockResult
          *
          * @return void
          */
-        private function getIBlockElements(
+        private function getIBlockReviewsElements(
             CIBlockResult $iBlockResult,
         ): void {
             while ($element = $iBlockResult->GetNextElement()) {
                 $id = $element->fields["ID"];
-                $this->arElements[$id] = $element->GetFields();
+                $this->arResult["ITEMS"][$id] = $element->GetFields();
             }
+        }
 
-            $this->navString = $this->iBlockResult->GetPageNavStringEx(
-                navComponentObject: $navComponentObject,
-                navigationTitle: '',
-                templateName: "modern",
-            );
+        /**
+         *------------------------------------------------------------------------------------------#
+         * Формирование постраничной навигации ↓
+         *---------------------------------------------------------------------------------------#
+         *
+         * @param  CIBlockResult  $iBlockResult
+         *
+         * @return void
+         */
+        private function getNavString(CIBlockResult $iBlockResult): void
+        {
+            $this->arResult["NAV_STRING"] =
+                $iBlockResult->GetPageNavStringEx(
+                    navComponentObject: $navComponentObject,
+                    navigationTitle: '',
+                    templateName: "modern",
+                );
+        }
 
+        /**
+         *------------------------------------------------------------------------------------------#
+         * Формирование списка полей элементов рейтинга ↓
+         *---------------------------------------------------------------------------------------#
+         *
+         * @return void
+         */
+        private function getRatingEnumFields(): void
+        {
             $ratingEnums = CIBlockPropertyEnum::GetList(
                 ["DEF" => "DESC", "SORT" => "DESC"],
-                ["IBLOCK_ID" => $this->iBlock["ID"], "CODE" => "RATING"],
+                ["IBLOCK_ID" => $this->iBlockReviews["ID"], "CODE" => "RATING"],
             );
 
             while ($enum_fields = $ratingEnums->GetNext()) {
                 $value = $enum_fields["VALUE"];
-                $this->ratingEnumFields[$value] = $enum_fields;
+                $this->arResult["RATING"][$value] = $enum_fields;
             }
         }
 
@@ -248,34 +283,35 @@
          * @param  HttpRequest  $request
          *
          * @return void
-         *
-         * @throws Exception
          */
         private function addComment(
             HttpRequest $request,
         ): void {
             $values = $request->getPostList()->toArray();
+
             if (!empty($values)) {
                 if (!empty($values["name"] && !empty($values["comment"]))) {
                     $values["name"] = $this->checkFormData($values['name']);
                     $values["comment"] =
                         $this->checkFormData($values['comment']);
-                    $this->arFieldsComment["IBLOCK_ID"] = $this->iBlock["ID"];
-                    $this->arFieldsComment["PREVIEW_TEXT"] = $values["name"];
+                    $this->commentFieldsIBlockReviews["IBLOCK_ID"] =
+                        $this->iBlockReviews["ID"];
+                    $this->commentFieldsIBlockReviews["PREVIEW_TEXT"] =
+                        $values["name"];
                     $values["name"] = sprintf("%s_%s", $values["name"], rand());
-                    $this->arFieldsComment["NAME"] = $values["name"];
+                    $this->commentFieldsIBlockReviews["NAME"] = $values["name"];
                     $code = CUtil::translit(
                         $values['name'],
                         "ru",
                         $this->translitCodeParams,
                     );
-                    $this->arFieldsComment["CODE"] = $code;
-                    $this->arFieldsComment["PROPERTY_VALUES"]["RATING"] =
+                    $this->commentFieldsIBlockReviews["CODE"] = $code;
+                    $this->commentFieldsIBlockReviews["PROPERTY_VALUES"]["RATING"] =
                         $values["rating"];
-                    $this->arFieldsComment["PROPERTY_VALUES"]["COMMENT"] =
+                    $this->commentFieldsIBlockReviews["PROPERTY_VALUES"]["COMMENT"] =
                         $values["comment"];
                     $oElement = new CIBlockElement();
-                    $oElement->Add($this->arFieldsComment);
+                    $oElement->Add($this->commentFieldsIBlockReviews);
                     echo "<meta http-equiv='refresh' content='0'>";
                 } else {
                     ShowError(
@@ -298,5 +334,18 @@
             $data,
         ): string {
             return htmlspecialchars(stripslashes(trim($data)));
+        }
+
+        /**
+         *------------------------------------------------------------------------------------------#
+         * Получаем экземпляр класса \Bitrix\Main\HttpRequest ↓
+         *---------------------------------------------------------------------------------------#
+         *
+         * @return \Bitrix\Main\HttpRequest
+         */
+        public function getCommentRequestObject(): HttpRequest
+        {
+            return Application::getInstance()->getContext()
+                ->getRequest();
         }
     }
